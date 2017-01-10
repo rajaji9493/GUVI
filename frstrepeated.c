f89dd6878796d34d37b4374f1d1f1f27d41bcfa3
Natarajan S <rajaji9493@gmail.com>
2017-01-10 05:46:44 +0530

    Create frstrepeated.c

---
+++ frstrepeated.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main(){
+	int n,a[100],i,j,f=0,r;
+	printf("Enter number of elements in array ");
+	scanf("%d",&n);
+	printf("Ennter array elements ");
+	for(i=0;i<n;i++)	
+	  scanf("%d",&a[i]);
+	printf("Repeated First : ");
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+	    	if(a[i]==a[j]){
+	       	 r=a[i];
+	      	 f=1;
+	      	 break;
+		  }
+	   }
+	   if(f==1)
+	     break;
+    }
+    printf("%d",r);	       
+}
